2b0f5af6588ec0fe5bad6e260daf3a3e63d0ea87
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2020-09-28 17:46:17 +1000

    Document origin of G_PRIME

--- src/secp256k1.rs
+++ src/secp256k1.rs
@@ -7,6 +7,8 @@ use marker::{Jacobian, Mark, NonZero, Secret, Zero};
 
 lazy_static::lazy_static! {
     /// Alternate generator of secp256k1.
+    ///
+    /// Obtained by hashing `ecdsa_fun::fun::G`. Originally used in Grin.
     pub static ref G_PRIME: Point =
         Point::from_bytes(hex_literal::hex!(
             "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0"
